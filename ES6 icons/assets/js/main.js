ca41f6aac37a550fe395e4374d16841f32d4f3d7
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-03-22 17:34:32 +0100

    Aggiornato il select

--- ES6 icons/assets/js/main.js
+++ ES6 icons/assets/js/main.js
@@ -16,20 +16,24 @@ $('.select').click(function(){
 
 
 $('#all').click(function(){
+    $('#all-active').text('All');
     $('li.animal, li.vegetable, li.user').show();
 })
 
 $('#animal').click(function(){
     $('li.animal, li.vegetable, li.user').show();
+    $('#all-active').text('Animal');
     $('li.vegetable, li.user').hide();
 })
 
 $('#vegetable').click(function(){
     $('li.animal, li.vegetable, li.user').show();
+    $('#all-active').text('Vegetable');
     $('li.animal, li.user').hide();
 })
 
 $('#user').click(function(){
     $('li.animal, li.vegetable, li.user').show();
+    $('#all-active').text('User');
     $('li.animal, li.vegetable').hide();
 })
